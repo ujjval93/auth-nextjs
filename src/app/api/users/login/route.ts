af6d03f3d3ce0e4ed9bd205fad9d74c3beac5a52
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-08-18 21:09:10 +0530

    dubgging error

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -10,22 +10,21 @@ export async function POST(request: NextRequest){
 
         const reqBody = await request.json()
         const {email, password} = reqBody;
-        //console.log(reqBody);
+        console.log(reqBody);
 
         //check if user exists
         const user = await User.findOne({email})
         if(!user){
             return NextResponse.json({error: "User does not exist"}, {status: 400})
         }
-        //console.log("user exists");
-        
+        console.log("user exists");
         
         //check if password is correct
         const validPassword = await bcryptjs.compare(password, user.password)
         if(!validPassword){
             return NextResponse.json({error: "Invalid password"}, {status: 400})
         }
-        //console.log(user);
+        console.log(user);
         
         //create token data
         const tokenData = {
--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -7,14 +7,14 @@ export async function POST(request: NextRequest){
 
     try {
         const reqBody = await request.json()
-        const {token} = reqBody;
+        const {token} = reqBody
 
         const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
 
         if (!user) {
             return NextResponse.json({error: "Invalid token"}, {status: 400})
         }
-        
+        console.log(user);
 
         user.isVerified = true;
         user.verifyToken = undefined;
